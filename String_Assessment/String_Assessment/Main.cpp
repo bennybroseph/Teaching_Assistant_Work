4c0df5d2806b321fc127ff0fa11288d343e08b5b
Benjamin Odom <bennybroseph@gmail.com>
2016-09-21 21:04:45 -0500

    Added Comments
    Added comments to code
    
    Check FindSubString function comments specifically for suggested changes
    to the header

--- String_Assessment/String_Assessment/Main.cpp
+++ String_Assessment/String_Assessment/Main.cpp
@@ -4,36 +4,66 @@
 using std::cout;
 using std::endl;
 
-int main(const char *args, int numArgs)
+using std::cin;
+
+int main(int numArgs, char *args)
 {
 	MyString string = "Default String";
 	cout << "Constructor:" << endl;
-	cout << string.CStyle() << endl << endl;
+	cout << string.CStyle() << endl;
+	cin.get();
+
+	int length = string.Length();
+	cout << "Length():" << endl;
+	cout << length << endl;
+	cin.get();
+
+	bool equal = string.Compare("Default String");
+	cout << "Compare(\"Default String\")" << endl;
+	cout << equal << endl;
+	cin.get();
 
 	string.Append(" Append");
-	cout << "Append Function:" << endl;
-	cout << string.CStyle() << endl << endl;
+	cout << "Append(\" Append\"):" << endl;
+	cout << string.CStyle() << endl;
+	cin.get();
 
 	string.Prepend("Prepend ");
-	cout << "Prepend Function:" << endl;
-	cout << string.CStyle() << endl << endl;
+	cout << "Prepend(\"Prepend \"):" << endl;
+	cout << string.CStyle() << endl;
+	cin.get();
 
 	string = string.ToLower();
-	cout << "ToLower Function:" << endl;
-	cout << string.CStyle() << endl << endl;
+	cout << "string = string.ToLower():" << endl;
+	cout << string.CStyle() << endl;
+	cin.get();
 
 	string = string.ToUpper();
-	cout << "ToUpper Function:" << endl;
-	cout << string.CStyle() << endl << endl;
+	cout << "string = string.ToUpper():" << endl;
+	cout << string.CStyle() << endl;
+	cin.get();
 
-	bool foundSubString = string.FindSubString("APPEND");
-	cout << "FindSubString Function:" << endl;
-	cout << foundSubString << endl << endl;
+	bool foundSubString = string.FindSubString("DEFAULT");
+	cout << "FindSubString(\"DEFAULT\"):" << endl;
+	cout << foundSubString << endl;
+	cin.get();
 
-	string.ReplaceSubString("APPEND", "Gone");
-	cout << "ReplaceSubString Function:" << endl;
-	cout << string.CStyle() << endl << endl;
+	foundSubString = string.FindSubString("DEFAULT", 15);
+	cout << "FindSubString(\"DEFAULT\", 15):" << endl;
+	cout << foundSubString << endl;
+	cin.get();
 
+	string.ReplaceSubString("PREPEND", "Gone");
+	cout << "ReplaceSubString(\"APPEND\", \"Gone\"):" << endl;
+	cout << string.CStyle() << endl;
+	cin.get();
+
+	string.ReplaceSubString("APPEND", "That's gone now too");
+	cout << "ReplaceSubString(\"APPEND\", \"That's gone now too\"):" << endl;
+	cout << string.CStyle() << endl;
+	cin.get();
+
+	cout << endl << "End of Program" << endl;
 	system("pause");
 
 	return 0;
--- String_Assessment/String_Assessment/MyString.h
+++ String_Assessment/String_Assessment/MyString.h
@@ -34,4 +34,5 @@ public:
 
 	void ReplaceSubString(MyString, MyString); //Replace a substring in the word with a new string
 };
+
 #endif // _MYSTRING_H_
